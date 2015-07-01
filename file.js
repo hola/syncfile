91e79e55900958ca25737212241bfb757cdc7c10
SirAnthony <anthony@adsorbtion.org>
2015-07-01 22:55:02 +0500

    Add files

---
+++ file.js
@@ -0,0 +1,293 @@
+// LICENSE_CODE ZON
+'use strict'; /*jslint node:true*/
+var crypto = require('crypto');
+var rimraf = require('rimraf');
+var path = require('path');
+var fs = require('fs');
+var E = exports;
+E.is_win = /^win/.test(process.platform);
+E.read_buf_size = 8192;
+E.safe = {};
+
+function check_file(dst, opt){
+    opt = opt||{};
+    if (opt.mkdirp)
+        E.mkdirp_file(dst);
+    if (opt.unlink)
+        E.unlink(dst);
+}
+function chomp_cr(str){
+    if (str[str.length-1]=='\r')
+        return str.substr(0, str.length-1);
+    return str;
+}
+
+E.read_line = function(file){
+    var fd, buf = '', eol_idx, res;
+    fd = fs.openSync(file, 'r');
+    while ((res = fs.readSync(fd, E.read_buf_size, null)) && res[1])
+    {
+	if ((eol_idx = res[0].indexOf('\n'))<0)
+	    buf += res[0];
+	else
+	{
+	    buf += res[0].substr(0, eol_idx);
+	    break;
+	}
+    }
+    fs.closeSync(fd);
+    buf = chomp_cr(buf);
+    return buf;
+};
+E.read_lines = function(file){
+    var res = fs.readFileSync(file, 'utf8');
+    var ret = res.split(/\r?\n/);
+    if (ret[ret.length-1]==='')
+        ret.pop();
+    return ret;
+};
+E.read = function(file, opt){
+    if (opt===undefined)
+        opt = 'utf8';
+    return fs.readFileSync(file, opt);
+};
+E.fread = function(fd, opt){
+    opt = opt||{};
+    var buf, res, ret = '';
+    var start = opt.start||0;
+    buf = new Buffer(E.read_buf_size);
+    // XXX: support for size
+    while ((res = fs.readSync(fd, buf, 0, E.read_buf_size, start)))
+    {
+        ret += buf.slice(0, res);
+        start += res;
+    }
+    return ret;
+};
+E.write = function(file, data, opt){
+    opt = opt||{};
+    check_file(file, opt);
+    fs.writeFileSync(file, data, opt);
+    return 0;
+};
+E.write_lines = function(file, data, opt){
+    return E.write(file,
+        Array.isArray(data) ? (data.length ? data.join('\n')+'\n' : '') :
+        ''+data+'\n', opt);
+};
+E.append = function(file, data, opt){
+    opt = opt||{};
+    check_file(file, opt);
+    fs.appendFileSync(file, data, opt);
+    return 0;
+};
+E.tail = function(file, count){
+    var fd, ret = '', start;
+    var stat = fs.statSync(file);
+    count = count||E.read_buf_size;
+    start = stat.size-count;
+    if (start<0)
+        start = 0;
+    fd = fs.openSync(file, 'r');
+    ret = E.fread(fd, {start: start});
+    fs.closeSync(fd);
+    return ret;
+};
+E._mkdirp = function(p, mode, made){
+    if (mode===undefined)
+        mode = parseInt('0777', 8) & ~(process.umask&&process.umask());
+    if (typeof mode==='string')
+        mode = parseInt(mode, 8);
+    if (!made)
+        made = null;
+    p = path.resolve(p);
+    var paths = [];
+    while (p && !E.exists(p))
+    {
+        paths.unshift(p);
+        p = path.dirname(p);
+    }
+    // Will throw all errors up
+    for (var i=0; i<paths.length; i++)
+    {
+        fs.mkdirSync(paths[i], mode);
+        made = made||paths[i];
+    }
+    return made||p;
+};
+E.mkdirp = function(p, mode){
+    if (mode===undefined || !process.umask)
+        return E._mkdirp(p);
+    var oldmask = process.umask(0);
+    try { return E._mkdirp(p, mode); }
+    finally { process.umask(oldmask); }
+};
+E.mkdirp_file = function(file){
+    E.mkdirp(path.dirname(file));
+    return file;
+};
+E.rm_rf = rimraf.sync;
+E.unlink = function(path){
+    try { fs.unlinkSync(path); }
+    catch(e){ return e.code; }
+};
+E.touch = function(path){
+    var fd = fs.openSync(path, 'a');
+    fs.closeSync(fd);
+};
+E.exists = function(path){
+    return fs.existsSync(path); };
+E.is_file = function(path){
+    var stat;
+    try { stat = fs.statSync(path); }
+    catch(e){ return false; }
+    return stat.isFile();
+};
+E.is_dir = function(path){
+    var stat;
+    try { stat = fs.statSync(path); }
+    catch(e){ return false; }
+    return stat.isDirectory();
+};
+E._copy = function(src, dst, opt){
+    var res, fd, fdw, pos = 0;
+    opt = opt||{};
+    // XXX: check if mode of dst file is correct after umask
+    try {
+        var stat = fs.statSync(src);
+        if (E.is_dir(dst)||dst[dst.length-1]=='/')
+            dst = dst+'/'+path.basename(src);
+        check_file(dst, opt);
+        fd = fs.openSync(src, 'r');
+        var mode = 'mode' in opt ? opt.mode : stat.mode & parseInt('0777', 8);
+        fdw = fs.openSync(dst, 'w', mode);
+    } catch(e){ return e.code; }
+    var buf = new Buffer(E.read_buf_size);
+    while ((res = fs.readSync(fd, buf, 0, E.read_buf_size, pos)))
+    {
+        fs.writeSync(fdw, buf, 0, res);
+        pos += res;
+    }
+    fs.closeSync(fd);
+    fs.closeSync(fdw);
+    return 0;
+};
+E._copy_dir = function(src, dst, opt){
+    var files = fs.readdirSync(src);
+    for (var f=0; f<files.length; f++)
+    {
+        var res = E.copy(src+'/'+files[f], dst+'/'+files[f], opt);
+        if (res)
+            return res;
+    }
+    return 0;
+};
+E.copy = function(src, dst, opt){
+    src = E.normalize(src);
+    if (E.is_dir(src))
+        return E._copy_dir(src, dst, opt);
+    return E._copy(src, dst, opt);
+};
+
+E.link = function(src, dst, opt){
+    var ret = 0;
+    opt = opt||{};
+    src = E.normalize(src);
+    dst = E.normalize(dst);
+    check_file(dst, opt);
+    try { fs.linkSync(src, dst); }
+    catch(e)
+    {
+        if (opt.no_copy)
+            return e.code;
+        return E.copy(src, dst, opt);
+    }
+    return 0;
+};
+
+E.symlink = function(src, dst, opt){
+    if (E.is_win)
+        return E.link(src, dst, opt);
+    src = E.normalize(src);
+    dst = E.normalize(dst);
+    check_file(dst, opt);
+    var ret = 0;
+    try {
+        var src_abs = fs.realpathSync(src);
+        fs.symlinkSync(src_abs, dst);
+    }
+    catch(e){ return e.code; }
+    return 0;
+};
+
+E.hashsum = function(filename, type){
+    var hash = crypto.createHash(type||'md5');
+    var fd = fs.openSync(filename, 'r');
+    var res, pos = 0, buffer = new Buffer(E.read_buf_size);
+    while ((res = fs.readSync(fd, buffer, 0, E.read_buf_size, pos)))
+    {
+        hash.update(buffer.slice(0, res));
+        pos += res;
+    }
+    fs.closeSync(fd);
+    return hash.digest('hex');
+};
+
+if (E.is_win)
+{
+    E.cygwin_root = E.is_dir('C:/cygwin') ? 'C:/cygwin' :
+	E.is_dir('D:/cygwin') ? 'D:/cygwin' : null;
+}
+E.cyg2unix = function(path){
+    if (!E.is_win)
+	return path;
+    // /cygdrive/X/yyy --> X:/yyy
+    path = path.replace(/^\/cygdrive\/(.)(\/(.*))?$/, "$1:/$3");
+    // /usr/lib --> c:/cygwin/lib
+    path = path.replace(/^\/usr\/lib\/(.*)?$/, E.cygwin_root+"/lib/$1");
+    // /usr/bin --> c:/cygwin/bin
+    path = path.replace(/^\/usr\/bin\/(.*)?$/, E.cygwin_root+"/bin/$1");
+    // /xxx --> c:/cygwin/xxx
+    path = path.replace(/^\//, E.cygwin_root.toLowerCase()+'/');
+    return path;
+};
+E.unix2win = function(path){
+    if (!E.is_win)
+	return path;
+    // c:/xxx -> C:/xxx
+    path = path.replace(/^[cd]:/, function(s){ return s.toUpperCase(); });
+    // C:/xxx/yyy -> C:\xxx\yyy
+    path = path.replace(/\//g, '\\');
+    return path;
+};
+E.win2unix = function(path, force)
+{
+    if (!force && !E.is_win)
+        return path;
+    // C:\xxx\yyy --> C:/xxx/yyy
+    path = path.replace(/\\/g, '/');
+    // C:/ --> c:/
+    path = path.replace(/^[cd]:/i, function(s){ return s.toLowerCase(); });
+    return path;
+};
+E.win2cyg = function(path){
+    if (!E.is_win)
+	return path;
+    path = E.win2unix(path);
+    var escaped_root = E.cygwin_root.replace(/([\?\\\/\[\]+*])/g, '\\$1');
+    path = path.replace(new RegExp("^"+escaped_root+"/?", "i"), '/');
+    path = path.replace(/^[cd]:/i, function(s){
+	return "/cygdrive/"+s[0].toLowerCase(); });
+    return path;
+};
+E.is_absolute = function(path){
+    return /^(\/|([cd]:))/i.test(path); };
+E.absolutize = function(p, d1, d2){
+    if (!p||E.is_absolute(p))
+        return p;
+    if (d2&&E.exists(d2+'/'+p))
+        return d2+'/'+p;
+    return d1+'/'+p;
+};
+E.normalize = function(p){
+    return E.cyg2unix(E.win2unix(path.normalize(p))); };
---
+++ test.js
@@ -0,0 +1,351 @@
+// LICENSE_CODE ZON
+'use strict'; /*jslint node:true, mocha:true*/
+var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+var file = require('./file.js');
+
+describe('file', function(){
+    var tmp_filename = 'test.tmp';
+    beforeEach(function(){ file.unlink(tmp_filename); });
+    afterEach(function(){ file.unlink(tmp_filename); });
+    it('read_line', function(){
+	var t = function(data, exp){
+	    var prev = file.read_buf_size;
+	    file.read_buf_size = 5;
+            file.write(tmp_filename, data);
+            assert.equal(file.read_line(tmp_filename), exp);
+	    file.read_buf_size = prev;
+	};
+	t('a', 'a');
+	t('ab\ncd\ne', 'ab');
+	t('ab\r\ncd\r\ne', 'ab');
+	t('abcde\ncd\ne', 'abcde');
+	t('abcdef\ncd\ne', 'abcdef');
+	t('abcde', 'abcde');
+	t('abcdef', 'abcdef');
+	t('abcdef\n', 'abcdef');
+	t('abcdef\r\n', 'abcdef');
+	assert.throws(file.read_line.bind(null, 'not_exist'), /ENOENT/);
+    });
+    it('read_lines', function(){
+	var t = function(data, exp){
+	    file.write(tmp_filename, data);
+	    assert.deepEqual(file.read_lines(tmp_filename), exp);
+	};
+	t('', []);
+	t('a', ['a']);
+	t('ab\ncd\ne', ['ab', 'cd', 'e']);
+	t('ab\ncd\ne\n', ['ab', 'cd', 'e']);
+	t('ab\ncd\ne\n\n', ['ab', 'cd', 'e', '']);
+	t('ab\r\ncd\r\ne\n\n', ['ab', 'cd', 'e', '']);
+	assert.throws(file.read_lines.bind(null, 'not_exist'), /ENOENT/);
+    });
+    it('read', function(){
+	var t = function(data, exp){
+	    file.write(tmp_filename, data);
+	    assert.deepEqual(file.read(tmp_filename), exp);
+	};
+	t('', '');
+	t('a', 'a');
+	t('ab\ncd\ne', 'ab\ncd\ne');
+	assert.throws(file.read.bind(null, 'not_exist'), /ENOENT/);
+    });
+    it('fread', function(){
+        var t = function(data, exp){
+            file.write(tmp_filename, data);
+            var fd = fs.openSync(tmp_filename, 'r');
+            assert.deepEqual(file.fread(fd), exp);
+            fs.close(fd);
+        };
+        t('', '');
+        t('a', 'a');
+        t('ab\ncd\ne', 'ab\ncd\ne');
+        assert.throws(file.fread.bind(null, -1), /EBADF/);
+    });
+    it('write', function(){
+	var t = function(data, exp){
+            file.write(tmp_filename, data);
+            assert.deepEqual(file.read(tmp_filename), exp);
+	};
+	t('', '');
+	t(1, '1');
+	t(new Buffer([1, 2, 3]), '\u0001\u0002\u0003');
+    });
+    it('write_lines', function(){
+	var t = function(data, exp){
+            file.write_lines(tmp_filename, data);
+            assert.deepEqual(file.read(tmp_filename), exp);
+	};
+	t('', '\n');
+	t(1, '1\n');
+	t([], '');
+	t(['abc'], 'abc\n');
+	t(['abc', 'def'], 'abc\ndef\n');
+    });
+    it('tail', function(){
+        var t = function(data, count, exp){
+            file.write(tmp_filename, data);
+            assert.equal(file.tail(tmp_filename, count), exp);
+        };
+        t('abcd', 3, 'bcd');
+        var ld = new Array(file.read_buf_size);
+        ld = ld.join('abc');
+        t(ld, null, ld.substr(ld.length-file.read_buf_size));
+    });
+    it('append', function(){
+        var expr = '';
+	var t = function(data){
+            expr += data;
+            file.append(tmp_filename, data);
+            assert.deepEqual(file.read(tmp_filename), expr);
+	};
+        t('', '');
+	t(1, '1');
+        t('23\n', '123\n');
+	t(new Buffer([1, 2, 3]), '123\n\u0001\u0002\u0003');
+    });
+    it('mkdirp', function(){
+        var t = function(dir, mode, root){
+            mode = mode||'0777';
+            var made = file.mkdirp(dir, mode);
+            assert(made);
+            // must return first created dir
+            assert(!dir.indexOf(made));
+            var bits = parseInt(mode, 8);
+            assert.equal(fs.statSync(dir).mode & bits, bits);
+	    file.rm_rf(made);
+        };
+        file.rm_rf('/tmp/mkdirp_test');
+	t('/tmp/mkdirp_test', '0777');
+	t('/tmp/mkdirp_test/test2/test3', '0755');
+	t('/tmp/mkdirp_test/a/b/c/d/_/r/e/c/u/r/s/i/v/e/_/m/k/d/i/r/_'+
+            '/f/a/i/l/e/d/_/w/i/t/h/_/t/h/i/s/');
+        assert.throws(function(){
+            var filename = '/tmp/mkdir_file';
+            file.touch(filename);
+            try { t(filename+'/cannot/create'); }
+            catch (e){ throw e; }
+            finally { file.unlink(filename); }
+        }, /ENOTDIR/);
+        assert.throws(t.bind(null, '/cannot/create'), /EACCES/);
+    });
+    it('exists', function(){
+        var t = function(_file, exp){
+            assert.equal(file.exists(_file), exp); };
+	t('.', true);
+	t('..', true);
+	t('does_not_exist', false);
+	t('test.js', true);
+	t('./test.js', true);
+    });
+    it('is_file', function(){
+        var t = function(_file, exp){
+            assert.equal(file.is_file(_file), exp); };
+	t('.', false);
+	t('..', false);
+	t('does_not_exist', false);
+	t('test.js', true);
+	t('./test.js', true);
+    });
+    it('is_dir', function(){
+        var t = function(dir, exp){
+            assert.equal(file.is_dir(dir), exp); };
+	t('.', true);
+	t('..', true);
+	t('does_not_exist', false);
+	t('test.js', false);
+	t('./test.js', false);
+    });
+    it('touch', function(){
+	assert.equal(file.touch(tmp_filename), undefined);
+	assert.equal(file.unlink(tmp_filename), undefined);
+	assert.throws(file.touch.bind(null, '/this/file/does-not/exist'),
+            /ENOENT/);
+    });
+    it('unlink', function(){
+	assert.equal(file.unlink('/this/file/does-not/exist'), 'ENOENT');
+	file.write(tmp_filename, 'abc');
+	assert.equal(file.unlink(tmp_filename), undefined);
+	assert.equal(file.unlink(tmp_filename), 'ENOENT');
+        // Unlink dir
+        file.mkdirp('test_dir');
+        assert.equal(file.unlink('test_dir'), 'EISDIR');
+        file.rm_rf('test_dir');
+    });
+    it('copy', function(){
+        var cp = 'copy_file';
+        var dcp = 'dst_file';
+        file.unlink(cp);
+        file.unlink(dcp);
+        function t(src, dst, data, mode, opt, ret){
+            mode = mode||'0666';
+            ret = ret||0;
+            if (typeof data=='string')
+                file.write(src, data, {mode: mode});
+            var result = file.copy(src, dst, opt);
+            assert.equal(result, ret);
+            if (!result)
+            {
+                if (file.is_dir(dst))
+                    dst += '/'+path.basename(src);
+                var statd = fs.statSync(dst);
+                assert.equal(statd.mode & parseInt('0777', 8),
+                    parseInt(mode, 8) & ~process.umask());
+                file.unlink(src);
+                file.unlink(dst);
+            }
+        }
+        t(cp, dcp, '');
+        t(cp, dcp, 'some data');
+        t(cp, dcp, 'more\ndata\n');
+        t(cp, dcp, 'data', '0777');
+        t('/file', dcp, null, 0, 0, 'ENOENT');
+        t(cp, '/file', 'wrong', 0, 0, 'EACCES');
+        var dcd = dcp+'_dir';
+        file.rm_rf(dcd);
+        t(cp, dcd+'/file', 'data', 0, 0, 'ENOENT');
+        t(cp, dcd+'/file', 'data', 0, {mkdirp: 1});
+        t(cp, dcd+'/', 'data');
+        t(cp, dcd, 'data');
+        t(cp, dcp, 'data', '0444', {mode: '444'});
+        file.unlink(cp);
+        file.unlink(dcp);
+        file.rm_rf(dcd);
+    });
+    it('link', function(){
+        var ls = 'link_src', ld = 'link_dst';
+        file.unlink(ls);
+        file.unlink(ld);
+        file.rm_rf(ld+'d');
+        var t = function(src, dst, opt){
+            assert(!file.link(src, dst, opt));
+            assert(file.exists(dst));
+            assert(file.is_file(dst));
+        };
+        file.write(ls, 'data');
+        t(ls, ld);
+        t(ls, ld+'d/dir/file', {mkdirp: 1});
+        file.unlink(ls);
+        file.unlink(ld);
+        file.rm_rf(ld+'d');
+    });
+    it('symlink', function(){
+        var ls = file.absolutize('slink_src', __dirname);
+        var ld = file.absolutize('slink_dst', __dirname);
+        file.unlink(ls);
+        file.unlink(ld);
+        file.rm_rf(ld+'d');
+        var t = function(src, dst, ret, opt){
+            var result = file.symlink(src, dst, opt);
+            assert.equal(result, ret||0);
+            if (result)
+                return;
+            var stat = fs.lstatSync(dst);
+            assert(stat.isSymbolicLink());
+            assert.equal(src, fs.readlinkSync(dst));
+        };
+        t(ls, ld, 'ENOENT');
+        file.write(ls, 'data');
+        t(ls, ld);
+        t(ls, ld, 'EEXIST');
+        t(ls, ld, 0, {unlink: 1});
+        t(ls, ld+'d/dir/file', 'ENOENT');
+        t(ls, ld+'d/dir/file', 0, {mkdirp: 1});
+        file.rm_rf(ld+'d');
+        file.unlink(ls);
+        file.unlink(ld);
+    });
+    it('hashsum', function(){
+        var f = 'hashsum_file';
+        var t = function(d, r, t){
+            file.write(f, d);
+            assert.equal(file.hashsum(f, t), r);
+        };
+        t('data', '8d777f385d3dfec8815d20f7496026dc');
+        t('data', '8d777f385d3dfec8815d20f7496026dc');
+        var ld = new Array(file.read_buf_size*3);
+        t(ld.join('1'), '0811769d134bdb1a36408c510d5e767a');
+        t(ld.join('1'), '0811769d134bdb1a36408c510d5e767a');
+        file.unlink(f);
+    });
+    it('is_absolute', function(){
+        var t = function(s, r){ assert(file.is_absolute(s)==r); };
+        t('/file', true);
+        t('file/not/absolute', false);
+        t('C:/dir', true);
+        t('win\\like', false);
+    });
+    it('absolutize', function(){
+        var t = function(s, d1, d2, r){
+            assert(file.absolutize(s, d1, d2)==r); };
+        t('/file', '/x', '', '/file');
+        t('file', '/dir', '', '/dir/file');
+        t('file', '/dir', '/', '/dir/file');
+        var cpf = '/tmp/abs_file';
+        var cp = path.dirname(cpf);
+        file.touch(cpf, '');
+        t('abs_file', '/', cp, cpf);
+        file.unlink(cpf);
+    });
+    it('normalize', function(){
+        var t = function(p, r){
+            assert(file.normalize(p)==r); };
+        t('file', 'file');
+        t('file/dir', 'file/dir');
+        t('file//dir', 'file/dir');
+        t('file///dir', 'file/dir');
+        t('file/./dir', 'file/dir');
+        t('./dir', 'dir');
+        t('dir/./', 'dir/');
+        t('dir/.', 'dir');
+        t('pre/dir/../file', 'pre/file');
+        t('dir/..', '.');
+        t('../dir', '../dir');
+    });
+    describe('cygwin', function(){
+        var is_win = file.is_win, root = file.cygwin_root;
+        before(function(){
+            file.is_win = true;
+            file.cygwin_root = 'C:/cygwin';
+        });
+        after(function(){
+            file.is_win = is_win;
+            file.cygwin_root = root;
+        });
+	it('cyg2unix', function(){
+	    var t = function(path, exp){
+		assert.equal(file.cyg2unix(path), exp); };
+	    t('file', 'file');
+	    t('dir/file', 'dir/file');
+	    t('c:/dir/file', 'c:/dir/file');
+	    t('/cygdrive/c/dir/file', 'c:/dir/file');
+	    t('/cygdrive/c', 'c:/');
+	    t('/dir', 'c:/cygwin/dir');
+            t('/', 'c:/cygwin/');
+	});
+        it('unix2win', function(){
+	    var t = function(path, exp){
+		assert.equal(file.unix2win(path), exp); };
+            t('file', 'file');
+            t('dir/file', 'dir\\file');
+            t('c:/dir/file', 'C:\\dir\\file');
+        });
+	it('win2unix', function(){
+	    var t = function(path, exp){
+		assert.equal(file.win2unix(path), exp); };
+	    t('file', 'file');
+	    t('dir\\file', 'dir/file');
+	    t('C:\\file', 'c:/file');
+        });
+        it('win2cyg', function(){
+	    var t = function(path, exp){
+		assert.equal(file.win2cyg(path), exp); };
+            t('C:\\file', '/cygdrive/c/file');
+	    t('C:\\cygwin\\usr', '/usr');
+	    t('C:/cygwin', '/');
+	    t('c:/cygwin/x', '/x');
+	    t('abC:/cygwinxx', 'abC:/cygwinxx');
+	});
+    });
+});
